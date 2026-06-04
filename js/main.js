9b79a73d78b6c3bc166782439ba9f4dc05f7be4c
0Shadow-sama0 <shadowsama51@gmail.com>
2026-06-04 18:28:19 -0500

    feat: Se actualizo el index (HEADER) y se creo la funcionalidad en el js- 0Shadow-sama0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 
 // ========== JAVASCRIPT COMPARTIDO ==========
 // Persona 3 puede agregar aquí el contador de partidos
-
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const destino = document.querySelector(this.getAttribute('href'));
+        if (destino) {
+            destino.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
 // ========== PERSONA 3: CONTADOR PRÓXIMO PARTIDO ==========
